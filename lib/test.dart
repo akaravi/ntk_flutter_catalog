af17a73c462ea7540d128cc425ba53d8a7d9105b
NegarIzadi <negar.izzadi@gmail.com>
2023-08-28 09:48:46 +0330

    edit3

---
+++ lib/test.dart
@@ -0,0 +1,69 @@
+            // Container(
+            //   width: double.infinity,
+            //   // margin: EdgeInsets.only(top: 0),
+            //   child: Image.asset(
+            //     'assets/images/360-Logo.png',
+            //     fit: BoxFit.fitHeight,
+            //     width: 220,
+            //     height: 220,
+            //   ),
+            // ),
+            // Text(
+            //   'ورود به ۳۶۰',
+            //   style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            // ),
+            // SizedBox(height: 20),
+            // Container(
+            //   alignment: Alignment.topRight,
+            //   margin: EdgeInsets.only(right: 40),
+            //   child: Text(
+            //     'تلفن همراه',
+            //     style: TextStyle(fontSize: 20),
+            //   ),
+            // ),
+            // Container(
+            //   margin: EdgeInsets.fromLTRB(60, 7, 60, 30),
+            //   child: TextField(
+            //     decoration: InputDecoration(
+            //         border: OutlineInputBorder(
+            //           borderRadius: BorderRadius.circular(30),
+            //           borderSide: BorderSide(),
+            //         ),
+            //         enabledBorder: OutlineInputBorder(
+            //           borderRadius: BorderRadius.circular(30),
+            //           borderSide:
+            //               BorderSide(color: Color.fromRGBO(254, 208, 1, 1)),
+            //         ),
+            //         hintText: '+۹۸ ۹ - - - - - - - - -',
+            //         hintStyle: TextStyle(fontSize: 22),
+            //         contentPadding: EdgeInsets.fromLTRB(60, 15, 60, 10)),
+            //   ),
+            // ),
+            // Container(
+            //   alignment: Alignment.topRight,
+            //   margin: EdgeInsets.only(right: 40),
+            //   child: Text(
+            //     'عبارت مقابل را وارد کنید',
+            //     style: TextStyle(fontSize: 10),
+            //   ),
+            // ),
+            // Row(
+            //   children: [
+            //     Container(
+            //       margin: EdgeInsets.fromLTRB(60, 7, 60, 30),
+            //       child: TextField(
+            //         decoration: InputDecoration(
+            //             border: OutlineInputBorder(
+            //               borderRadius: BorderRadius.circular(30),
+            //               borderSide: BorderSide(),
+            //             ),
+            //             enabledBorder: OutlineInputBorder(
+            //               borderRadius: BorderRadius.circular(30),
+            //               borderSide:
+            //                   BorderSide(color: Color.fromRGBO(254, 208, 1, 1)),
+            //             ),
+            //             hintText: '- - - - -',
+            //             hintStyle: TextStyle(fontSize: 22),
+            //             contentPadding: EdgeInsets.fromLTRB(60, 15, 60, 10)),
+            //       ),
+            //     ),
